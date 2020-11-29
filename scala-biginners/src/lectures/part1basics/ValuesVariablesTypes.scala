0d64171ec7b4bf6587fecceb54971be83e62ccb7
ariel7342 <ariell@wix.com>
2020-11-29 12:31:51 +0200

    examples of val and var and a bit of types

--- scala-biginners/src/lectures/part1basics/ValuesVariablesTypes.scala
+++ scala-biginners/src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -1,8 +1,22 @@
 package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
-  val x: Int = 42
+  val x = 42
   println(x)
 
   // vals are immutable (ike const)
+  // compiler can infer types
+
+  val aString: String = "Hello"
+
+  val ABoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aShort: Short = 4444
+  val aLong: Long = 555555555555555L
+  val aFloat: Float = 2.0f
+  val ADouble: Double = 3.14
+
+  //variables
+  var aVariable: Int = 4
+  aVariable = 5 // side effects
 }
